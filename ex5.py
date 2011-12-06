9769efd164ef8fb6767acba2930dece9e7ee1460
Brent Yingling <brent.yingling31@gmail.com>
2011-12-06 09:49:18 -0600

    ex5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %r inches tall." % height
+print "He's %r pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %r, %r, and %r I get %r." % (age, height, weight, age + height + weight)
