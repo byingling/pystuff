1e7586bb011a5283ac1759c80f86bd012e7ca8d0
Brent Yingling <brent.yingling31@gmail.com>
2011-12-06 07:07:37 -0600

    files

---
+++ ex3.py
@@ -0,0 +1,21 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6   # adds then divides
+print "Roosters", 100 - 25 * 3 % 4   # subtract then multiply
+
+print "Now I will count the eggs."
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6     # adds everything
+
+print "Is that true that 3 + 2 < 5 - 7?"    # tells true or false
+
+print "What is 3 + 2?", 3 + 2   # adds set numbers
+print "What is 5 - 7?", 5 - 7   # subtracts set numbers
+
+print "Oh, that's why it's False."
+
+print "How about one more."
+
+print "Is it greater?", 5 > -2      # answers < or > true or false
+print "Is it greater or equal?", 5 >= -2    # aswers < or > true of false
+print "Is it less or equal?", 5 <= -2   # answers < or > true or false
