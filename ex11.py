f9c148d718dec17ad94d1782a442eaaaebd191b0
Brent Yingling <brent.yingling31@gmail.com>
2011-12-06 21:04:33 -0600

    ex files

---
+++ ex11.py
@@ -0,0 +1,18 @@
+print "How old are you?",
+age = raw_input ()
+print "How tall are you>",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." %(
+    age, height, weight)
+
+print "Do you have any kids?"
+kids = raw_input()
+print "Boys or girls?"
+both = raw_input()
+boys = raw_input() 
+girls =  raw_input()
+
+print "You have %r kids, %r and %r." %(kids, boys, girls)
